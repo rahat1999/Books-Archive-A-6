c0888e58f5e607edf84f4fffc89149d1b1f5d1fc
rahat1999 <rahat.is.1999@gmail.com>
2021-09-02 19:09:18 +0600

    done

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
+/*-----global-------*/
 const getBooks = document.getElementById('get-books');
-const searchInput = document.getElementById('search-input');
+const inputField = document.getElementById('input-field');
 document.getElementById('spinner').style.display='none';
+
 const loadBookArchive = () => {
-    const searchByName = searchInput.value;
+    const searchByName = inputField.value;
         // error masssage;
         if(searchByName ===''){
             document.getElementById('errors').innerText='Search Field Empty';
+            document.getElementById('total').innerText='';
+            getBooks.innerHTML=''
         }
         else{
             /*---show spinner---*/
@@ -16,37 +20,39 @@ const loadBookArchive = () => {
             const url = `http://openlibrary.org/search.json?q=${searchByName}`
             fetch(url)
                 .then(res => res.json())
-                .then(data =>displayBookArchive(data.docs))
-                /*----clear---*/
-                searchInput.value=""
+                .then(data =>displayBookArchive(data))  
+                /*----clear dom---*/
+                document.getElementById('total').innerText=''
+                inputField.value=""
                 document.getElementById('get-books').innerHTML='';
         }
     }
 
-        const displayBookArchive = books => {
-         /* hide spinner */
+        const displayBookArchive = data => {
+            /*---- total result----*/
+            document.getElementById('total').innerText=`Total Result Found: ${data.numFound}`
+            const books =data.docs
+         /* ------hide spinner------- */
          document.getElementById('spinner').style.display='none';
-       /*----each book total----*/
-        document.getElementById('total').innerText=`Total: ${books.length} Books`
         if(books.length===0){
-            document.getElementById('errors').innerText='No Result Found'
+            document.getElementById('errors').innerText='No Result Found ☠';
         }
         else{
             books.forEach(book => {
                 document.getElementById('errors').innerText='';
                 const div = document.createElement('div');
                 div.classList.add('col')
-                div.innerHTML = `
-                    <div class="card h-100">
-                    <img  src="https://covers.openlibrary.org/b/id/${book?.cover_i?book?.cover_i:554106}-M.jpg" class="card-img-top h-75 bg-success" alt="Books image">
-                    <div class="card-body bg-dark text-warning">
-                    <p class="card-title">Book Name : <b>${book?.title?book.title:'N/a'}</b></p>
-                    <p>Authour Name : <b>${book.author_name}</b></p>
-                    <p>Publisher : <b>${book.publisher}</b></p>
-                    <p>First Publish Year : <b>${book?.first_publish_year?book.first_publish_year:'N/a'}</b></p>
+                div.innerHTML =`
+                <div class="card h-100 p-2 rounded-2">
+                        <img src="https://covers.openlibrary.org/b/id/${book.cover_i?book.cover_i:554106}-M.jpg" class="card-img-top" alt="...">
+
+                    <div class="card-body bg-secondary">
+                        <li class="card-text text-light">Book Name : <b class="text-warning">${book?.title?book.title:'N/a'}</li>
+                        <li class="card-text text-light">Authour Name: <b class="text-warning">${book.author_name}</li>
+                        <li class="card-text text-light">First Publish Year : <b class="text-warning">${book?.first_publish_year?book.first_publish_year:'N/a'}<b></li>
                     </div>
-                </div>
-                    `
+                </div>`
+                
                 getBooks.appendChild(div);
         
             });
