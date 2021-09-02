630a96cc5f9d2d58e8e16de263af79fef64aeb62
rahat1999 <rahat.is.1999@gmail.com>
2021-09-02 07:30:04 +0600

    second commit

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
-const getBooks = document.getElementById('get-books')
+const getBooks = document.getElementById('get-books');
 const searchInput = document.getElementById('search-input');
 
 const loadBookArchive = () => {
     const searchByName = searchInput.value;
-    // error masssage;
-    if(searchByName ===''){
-        document.getElementById('errors').innerText='Search Field Empty'
-    }
-    else{
-
-        document.getElementById('errors').innerText=''
-        const url = `http://openlibrary.org/search.json?q=${searchByName}`
-        fetch(url)
-            .then(res => res.json())
-            .then(data =>displayBookArchive(data.docs))
-          /*----clear input tex-t---*/
-            searchInput.value=""
-            document.getElementById('get-books').innerHTML=''
-    }
+        // error masssage;
+        if(searchByName ===''){
+            document.getElementById('errors').innerText='Search Field Empty';
+        }
+        else{
+            document.getElementById('errors').innerText=''
+            const url = `http://openlibrary.org/search.json?q=${searchByName}`
+            fetch(url)
+                .then(res => res.json())
+                .then(data =>displayBookArchive(data.docs))
+                /*----clear---*/
+                searchInput.value=""
+                document.getElementById('get-books').innerHTML=''
+        }
     }
     
 
 const displayBookArchive = books => {
-    // console.log(books)
+       /*----each book total----*/
+        document.getElementById('total').innerText=`Total: ${books.length} Books`
         if(books.length===0){
-            document.getElementById('errors').innerText='Result not Found'
+            document.getElementById('errors').innerText='No Result Found'
         }
         else{
             books.forEach(book => {
@@ -33,11 +33,12 @@ const displayBookArchive = books => {
                 div.classList.add('col')
                 div.innerHTML = `
                     <div class="card h-100">
-                    <img  src="https://covers.openlibrary.org/b/id/${book?.cover_i?book?.cover_i:554106}-M.jpg" class="card-img-top h-75" alt="Books image">
-                    <div class="card-body bg-dark text-warning text-center">
-                    <h5 class="card-title">${book.title}</h5>
-                    <h5>${book.author_name}</h5>
-                    <h5>${book.first_publish_year}</h5>
+                    <img  src="https://covers.openlibrary.org/b/id/${book?.cover_i?book?.cover_i:554106}-M.jpg" class="card-img-top h-75 bg-success" alt="Books image">
+                    <div class="card-body bg-dark text-warning">
+                    <p class="card-title">Book Name : <b>${book?.title?book.title:'N/a'}</b></p>
+                    <p>Authour Name : <b>${book.author_name}</b></p>
+                    <p>Publisher : <b>${book.publisher}</b></p>
+                    <p>First Publish Year : <b>${book?.first_publish_year?book.first_publish_year:'N/a'}</b></p>
                     </div>
                 </div>
                     `
